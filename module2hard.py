989f611aba11b77f82232c37104cf8ad5c08f350
Vasco Bechterev <166926411+Vasveshiy@users.noreply.github.com>
2024-07-17 22:27:36 +0500

    24.07.17 - Модуль 2
    Дополнительное практическое задание по модулю: "Основные операторы"

---
+++ module2hard.py
@@ -0,0 +1,31 @@
+def check():
+    while True:
+        num = int(input('Введите число от 3 до 20: '))
+        if 3 <= num <= 20:
+            return num
+        else:
+            print("Вы ввели некорректное число, повторите попытку.")
+
+
+number = check()
+unique_pairs = set()  # Множество для отслеживания уникальных пар
+list = []
+
+print('Ваше число:', number)
+
+for i in range(1, number + 1):
+    for j in range(1, number + 1):
+        if i != j:  # Исключаем сумму числа с самим собой
+            total_sum = i + j
+            if total_sum % number == 0:
+                pair = (min(i, j), max(i, j))  # Создаем отсортированный кортеж
+                if pair not in unique_pairs:
+                    unique_pairs.add(pair)
+                    list.append(pair)
+
+# Преобразование списка кортежей в строку и удаление скобок
+#list_str = (str(list).replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace(' ', ''))
+
+#list_str = str(list).replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace(' ', '')
+list_str = str(list).remove('(')
+print('Ваш шифр:', list_str)
