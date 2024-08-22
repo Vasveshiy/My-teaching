dfa9f5c6675ed1194cb5b9b0c89d67575e7fb0a4
Vasco Bechterev <166926411+Vasveshiy@users.noreply.github.com>
2024-08-22 22:29:25 +0500

    24.08.22 - Модуль 5, задание 4
    Домашняя работа по уроку "Различие атрибутов класса и экземпляра."

---
+++ module_5_4.py
@@ -0,0 +1,77 @@
+class House:
+    houses_history = []  # Атрибут класса для хранения истории домов
+
+    def __new__(cls, *args, **kwargs):
+        # Добавляем название дома в историю
+        instance = super().__new__(cls)  # Создание нового экземпляра
+        cls.houses_history.append(args[0])  # args[0] — это название дома
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __del__(self):
+        # Выводим сообщение при удалении объекта
+        print(f"{self.name} снесён, но он останется в истории")
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        else:
+            return False
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        if isinstance(value, House):
+            return House(self.name, self.number_of_floors + value.number_of_floors)
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
